b21637d416cf7417d9e1c17badb355a74ae4e558
YuriySW <yuriy.s@my.com>
2026-02-06 00:19:45 +0500

    vercel1.7

--- api/index.js
+++ api/index.js
@@ -2,15 +2,12 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   try {
-    const response = await fetch(
-      'https://api.pushshift.io/reddit/search/submission?sort=desc&size=10',
-      {
-        headers: {
-          // КЛЮЧЕВОЕ МЕСТО
-          'User-Agent': 'blogget-educational-project/1.0',
-        },
+    const response = await fetch('https://api.reddit.com/best.json?limit=10', {
+      headers: {
+        // КЛЮЧЕВОЕ МЕСТО
+        'User-Agent': 'blogget-educational-project/1.0',
       },
-    );
+    });
 
     if (!response.ok) {
       return res.status(response.status).json({
