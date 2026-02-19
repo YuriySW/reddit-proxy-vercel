d7a5b22dda02a5a7021428c0f29d313bb9884905
YuriySW <yuriy.s@my.com>
2026-02-19 15:24:50 +0500

    test16

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   try {
     const url = new URL(req.url, 'http://localhost');
 
-    const redditUrl = `https://www.reddit.com${url.pathname}${url.search}`;
+    const redditUrl = `https://api.reddit.com${url.pathname}${url.search}`;
 
     console.log('Proxying to:', redditUrl);
 
